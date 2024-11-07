947f3be51044f1361ce1ae79c036c23466370735
gerasantillan17 <gerapumas122@gmail.com>
2024-11-07 00:07:16 -0600

    Add files via upload

---
+++ JavaApplication54.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication54;
+
+/**
+ *
+ * @author gerar
+ */
+public class JavaApplication54 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int []arreglo = new int[10];
+        arreglo[0] = 100;
+        arreglo[9] = 900;
+        System.out.println(arreglo[0]);
+        System.out.println(arreglo[1]);
+        System.out.println(arreglo[2]);
+        System.out.println(arreglo[3]);
+        System.out.println(arreglo[4]);
+        System.out.println(arreglo[5]);
+        System.out.println(arreglo[6]);
+        System.out.println(arreglo[7]);
+        System.out.println(arreglo[8]);
+        System.out.println(arreglo[9]);
+    }
+    
+    
+}
